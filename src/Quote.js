172c27812d62b1b3dfed479d17d69f195f4942e4
Lena <summyalena@gmail.com>
2022-09-14 20:48:06 +0100

    done with the spa

---
+++ src/Quote.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import React from 'react'
+import Headers from './components/Headers';
+import './quote.css'
+
+function Quote() {
+  return (
+    <>
+    <Headers />
+    <div className='quote'>
+     
+        <p>Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding. - William Paul Thurston</p>
+    </div>
+    </>
+  )
+}
+
+export default Quote;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import NewCalculator from './components/newCalculator';
+import Quote from './Quote';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/calculator" element={<NewCalculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
